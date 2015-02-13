9d16f12073fe102520e237eac60106a2c0ce4305
niku <niku@niku.name>
2015-02-14 06:49:34 +0900

    すべての端末で画面サイズを320 * 480として扱う

--- main.js
+++ main.js
@@ -49,7 +49,7 @@
 
 cc.game.onStart = function(){
     cc.view.adjustViewPort(true);
-    cc.view.setDesignResolutionSize(800, 450, cc.ResolutionPolicy.SHOW_ALL);
+    cc.view.setDesignResolutionSize(320, 480, cc.ResolutionPolicy.SHOW_ALL);
     cc.view.resizeWithBrowserSize(true);
     //load resources
     cc.LoaderScene.preload(g_resources, function () {
