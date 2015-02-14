3b506dc30c8acc807a20f9d2acc87cd9663a819b
niku <niku@niku.name>
2015-02-15 07:14:22 +0900

    使用しないリソースを削除する

--- src/resource.js
+++ src/resource.js
@@ -1,7 +1,4 @@
 var res = {
-    HelloWorld_png : "res/HelloWorld.png",
-    CloseNormal_png : "res/CloseNormal.png",
-    CloseSelected_png : "res/CloseSelected.png",
     background: "res/images/background.png"
 };
 
